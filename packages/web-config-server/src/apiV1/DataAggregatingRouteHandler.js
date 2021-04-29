61f86a128f0ff9865a531c73e0cccd213930f55c
Rohan Port <rohan@beyondessential.com.au>
2021-04-30 09:26:33 +1000

    2697: Added tupaia-server user to act as public user for web-config-server

--- packages/web-config-server/src/apiV1/DataAggregatingRouteHandler.js
+++ packages/web-config-server/src/apiV1/DataAggregatingRouteHandler.js
@@ -4,9 +4,18 @@
  */
 
 import { createAggregator } from '@tupaia/aggregator';
+import { createBasicHeader } from '@tupaia/utils';
 import { RouteHandler } from './RouteHandler';
 import { Aggregator } from '/aggregator';
 import { refreshAndSaveAccessToken } from '/appServer/requestHelpers/refreshAndSaveAccessToken';
+
+const { MICROSERVICE_CLIENT_USERNAME, MICROSERVICE_CLIENT_SECRET } = process.env;
+
+const PUBLIC_USER_NAME = 'public';
+const PUBLIC_USER_AUTH_HEADER = createBasicHeader(
+  MICROSERVICE_CLIENT_USERNAME,
+  MICROSERVICE_CLIENT_SECRET,
+);
 
 /**
  * Interface class for handling routes that fetch data from an aggregator
@@ -16,10 +25,13 @@ export class DataAggregatingRouteHandler extends RouteHandler {
   constructor(req, res) {
     super(req, res);
 
-    // TODO: How to support public user???
     const userName = req.session?.userJson?.userName;
 
     const getAuthHeader = async () => {
+      if (userName === PUBLIC_USER_NAME) {
+        return PUBLIC_USER_AUTH_HEADER;
+      }
+
       const { refreshToken } = await req.models.userSession.findOne({
         userName,
       });
